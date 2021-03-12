71a88d4c7e37bdd51363ce41a5745c3c75790321
Anirudh Babu <anirudhbabu2001@gmail.com>
2021-03-11 19:54:18 -0500

    Project cards retrieved from DB & Displayed
    The DB has been accessed to display stored projects. Created by admin, these projects
    are only for testing purposes.

--- teammy/teammy/CreateProject.xaml.cs
+++ teammy/teammy/CreateProject.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,49 @@ namespace teammy
     /// </summary>
     public partial class CreateProject : Window
     {
+        public string connectionString = @"server=db-mysql-tor1-21887-do-user-8838717-0.b.db.ondigitalocean.com; database=teammy; uid=admin; pwd=sxx0uix39f5ty52d; port=25060;";
+        Color[] backColors = new Color[] { Colors.Red, Colors.Blue, Colors.Orange, Colors.Aqua, Colors.BlueViolet, Colors.Gold, Colors.Brown, Colors.Coral };
+        List<string> ProjectNames = new List<string>();
+
         public CreateProject()
         {
             InitializeComponent();
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT Proj_Name FROM projects", conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            using (reader)
+            {
+                ProjectBox project;
+                int left = 44, top = 13, right = 361, bottom = 217;
+                int count = 0;
+                Random rd = new Random();
+                string projName;
+                string profChars;
+
+                while (reader.Read())
+                {
+                    projName = reader[0].ToString();
+                    ProjectNames.Add(reader[0].ToString());
+                    profChars = projName.Split(' ')[0][0] + "" + projName.Split(' ')[1][0];
+                    project = new ProjectBox() { ProjectName = projName, Margin = new Thickness(left, top, right, bottom), ProjectProfileBack= backColors[rd.Next(0, 7)], ProjectProfile=profChars };
+                    projGrid.Children.Add(project);
+                    left += 135;
+                    right -= 135;
+                    
+                    if(count == 2)
+                    {
+                        top = 145;
+                        bottom = 85;
+                        left = 44;
+                        right = 361;
+                        count = 0;
+                    }
+                    count++;
+                }
+            }
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
