e2fc073090f13c78f3bc36b7a1017119b6e90b78
Ravleen Kaur <ravleenkaurtumber@gmail.com>
2021-03-13 21:45:48 -0500

    Menu items stylized
    The icons of the menu have been specially modified to as if they are also
    hovered upon.

--- teammy/teammy/CreateProject.xaml.cs
+++ teammy/teammy/CreateProject.xaml.cs
@@ -88,12 +88,43 @@ namespace teammy
 
         private void btnMenu_Click(object sender, RoutedEventArgs e)
         {
-
-        }
+            ContextMenu cm = this.FindResource("cmButton") as ContextMenu;
+            cm.PlacementTarget = sender as Button;
+            cm.IsOpen = true;
+ }
 
         private void pnlTitle_MouseDown(object sender, MouseEventArgs e)
         {
             DragMove();
+        }
+
+        /// <summary>
+        ///     The method sets the background of the Button it contains to the same color as if it were hovered 
+        ///     upon.
+        /// </summary>
+        /// <param name="sender">The MenuItem triggering this event</param>
+        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
+
+            //The Button whose background is to be set
+            Button btnIcon = MenuItem.Children[1] as Button;
+            btnIcon.Background = new SolidColorBrush(Colors.LightBlue) { Opacity= 0.7};
+        }
+
+        /// <summary>
+        ///     The method sets the background of the Button it contains to the same color as if it lost focus.
+        /// </summary>
+        /// <param name="sender">The MenuItem triggering this event</param>
+        private void MenuItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
+
+            //The Button whose background is to be set
+            Button btnIcon = MenuItem.Children[1] as Button;
+            btnIcon.Background = null;
         }
     }
 }
