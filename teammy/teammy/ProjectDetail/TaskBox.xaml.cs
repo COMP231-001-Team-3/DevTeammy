1f94c97a841e9dc741b7eb85c210580d79a99bd3
Anirudh Babu <anirudhbabu2001@gmail.com>
2021-03-16 13:55:29 -0400

    Design improvements to status & priority
    Added gitignore

--- teammy/teammy/ProjectDetail/TaskBox.xaml.cs
+++ teammy/teammy/ProjectDetail/TaskBox.xaml.cs
@@ -23,104 +23,7 @@ namespace teammy.ProjectDetail
     {
         public TaskBox()
         {
-            
-            this.DataContext = this;
-            StatusImages = new ObservableCollection<PicClass>();
-            PriorityImages = new ObservableCollection<PicClass>();
-            AssigneeImages = new ObservableCollection<PicClass>();
-
             InitializeComponent();
-
-            //StatusImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\notStarted.png")),
-            //    //ImgName = "Not started"
-
-            //});
-            //StatusImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\underProgress.png")),
-            //    //ImgName = "Under Progress"
-
-            //});
-            //StatusImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\done.png")),
-            //    //ImgName = "Done"
-
-            //});
-
-            //PriorityImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\priority 1.png")),
-            //    //ImgName = "High"
-
-            //});
-            //PriorityImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\priority2.png")),
-            //    //ImgName = "Midium"
-
-            //});
-            //PriorityImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\priority3.png")),
-            //    //ImgName = "Low"
-
-            //});
-
-            //AssigneeImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\priority 1.png")),
-            //    ImgName = "assignee1"
-
-            //});
-            //AssigneeImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\priority2.png")),
-            //    ImgName = "assignee2"
-
-            //});
-            //AssigneeImages.Add(new PicClass
-            //{
-            //    Img = new BitmapImage(new Uri(@"C:\Users\user\Desktop\Programming 3\comp231-001_team3\teammy\teammy\images\priority3.png")),
-            //    ImgName = "assignee3"
-
-            //});
-        }
-
-        public ObservableCollection<PicClass> StatusImages { get; set; }
-        public ObservableCollection<PicClass> PriorityImages { get; set; }
-        public ObservableCollection<PicClass> AssigneeImages { get; set; }
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void btnDetails_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)         //when you click duedate button
-        {
-            
-
-        }
-
-        private void assigneeCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
         }
 
         private void btnPriority_Click(object sender, RoutedEventArgs e)
@@ -132,26 +35,55 @@ namespace teammy.ProjectDetail
 
         private void btnStatus_Click(object sender, RoutedEventArgs e)
         {
-            ContextMenu cm = FindResource("cmPriority") as ContextMenu;
+            ContextMenu cm = FindResource("cmStatus") as ContextMenu;
             cm.PlacementTarget = sender as Button;
             cm.IsOpen = true;
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void PriorMenuItem_Click(object sender, RoutedEventArgs e)
         {
             Brush chosenPriority = ((sender as MenuItem).Icon as Rectangle).Fill;
             priorityGrid.Background = chosenPriority;
         }
-    }
 
+        private void StatusMenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
 
-    public class PicClass
-    {
+            //The Button whose background is to be set
+            Button btnIcon = MenuItem.Children[1] as Button;
+            btnIcon.Background = new SolidColorBrush(Colors.LightBlue) { Opacity = 0.7 };
+        }
 
-        public BitmapImage Img { get; set; }
-        public string ImgName { get; set; }
+        private void StatusMenuItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
 
+            //The Button whose background is to be set
+            Button btnIcon = MenuItem.Children[1] as Button;
+            btnIcon.Background = null;
+        }
 
+        private void StatusMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
+
+            ImageSource iconSource = (MenuItem.Children[0] as Image).Source;
+            statusGrid.Background = new ImageBrush(iconSource);
+        }
+
+        private void btnAssignee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void assigneeCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
 
