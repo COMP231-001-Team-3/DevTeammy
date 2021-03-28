b257167b128d38fc7a751e96e3c92468917bdb1a
Anirudh Babu <anirudhbabu2001@gmail.com>
2021-03-28 14:21:56 -0400

    Initialized page layout

---
+++ teammy/teammy/ProgressReport.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Collections.Generic;
+using LiveCharts;
+using LiveCharts.Charts;
+using LiveCharts.Wpf;
+using LiveCharts.Defaults;
+
+namespace teammy
+{
+    /// <summary>
+    /// Interaction logic for CreateProject.xaml
+    /// </summary>
+    public partial class ProgressReport : Window
+    {
+
+        #region Fields
+        //Hosted DB connection string
+        private string connectionString = @"server=db-mysql-tor1-21887-do-user-8838717-0.b.db.ondigitalocean.com; database=teammy; uid=admin; pwd=sxx0uix39f5ty52d; port=25060;";
+        ProjectBox toBeInserted;
+        MySqlConnection conn;
+        TextBox txtNameInput;
+        #endregion
+
+        //public UserModel currentUser { get; set; } = Application.Current.Resources["currentUser"] as UserModel;
+        public SeriesCollection PieSeries { get; set; } = new SeriesCollection()
+        {
+            new PieSeries
+            {
+                Title = "Not started",
+                Values = new ChartValues<ObservableValue> { new ObservableValue(20) },
+                DataLabels = true
+            },
+            new PieSeries
+            {
+                Title = "In Progress",
+                Values = new ChartValues<ObservableValue> { new ObservableValue(20) },
+                DataLabels = true
+            },
+            new PieSeries
+            {
+                Title = "Completed",
+                Values = new ChartValues<ObservableValue> { new ObservableValue(20) },
+                DataLabels = true
+            }
+        };
+
+        public ColorsCollection PieColors { get; set; } = new ColorsCollection()
+        {
+            Colors.Red,
+            Colors.RoyalBlue,
+            Colors.MediumSeaGreen
+        };
+
+        #region Constructor
+        public ProgressReport()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Title Bar Button Event Handlers
+
+        /// <summary>
+        ///     Shuts down the application
+        /// </summary>
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        ///     Minimizes the current window
+        /// </summary>
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ContextMenu cm = FindResource("cmButton") as ContextMenu;
+            cm.PlacementTarget = sender as Button;
+            cm.IsOpen = true;
+        }
+        #endregion
+
+        #region Title Pane Event Handlers
+
+        /// <summary>
+        ///     Moves the window along with the title pane when it is dragged
+        /// </summary>
+        private void pnlTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            DragMove();
+        }
+
+        /// <summary>
+        ///     The method sets the background of the Button it contains to the same color as if it were hovered 
+        ///     upon.
+        /// </summary>
+        /// <param name="sender">The MenuItem triggering this event</param>
+        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
+
+            //The Button whose background is to be set
+            Button btnIcon = MenuItem.Children[1] as Button;
+            btnIcon.Background = new SolidColorBrush(Colors.LightBlue) { Opacity = 0.7 };
+        }
+
+        /// <summary>
+        ///     The method sets the background of the Button it contains to the same color as if it lost focus.
+        /// </summary>
+        /// <param name="sender">The MenuItem triggering this event</param>
+        private void MenuItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
+
+            //The Button whose background is to be set
+            Button btnIcon = MenuItem.Children[1] as Button;
+            btnIcon.Background = null;
+        }
+        #endregion
+
+        private void homeMenu_click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            (Application.Current.Resources["mainInstance"] as Window).Show();
+        }
+    }
+}
