681ef720c9d22d2baa6f73121a5eda1f7cab427c
Anirudh Babu <anirudhbabu2001@gmail.com>
2021-03-28 18:28:36 -0400

    Project based progress report

--- teammy/teammy/ProgressReport.xaml.cs
+++ teammy/teammy/ProgressReport.xaml.cs
@@ -10,6 +10,7 @@ using LiveCharts;
 using LiveCharts.Charts;
 using LiveCharts.Wpf;
 using LiveCharts.Defaults;
+using System.Threading.Tasks;
 
 namespace teammy
 {
@@ -21,31 +22,29 @@ namespace teammy
 
         #region Fields
         //Hosted DB connection string
-        private string connectionString = @"server=db-mysql-tor1-21887-do-user-8838717-0.b.db.ondigitalocean.com; database=teammy; uid=admin; pwd=sxx0uix39f5ty52d; port=25060;";
-        ProjectBox toBeInserted;
-        MySqlConnection conn;
-        TextBox txtNameInput;
+        private static string connectionString = @"server=db-mysql-tor1-21887-do-user-8838717-0.b.db.ondigitalocean.com; database=teammy; uid=admin; pwd=sxx0uix39f5ty52d; port=25060;";
+        MySqlConnection conn = new MySqlConnection(connectionString);
         #endregion
 
         //public UserModel currentUser { get; set; } = Application.Current.Resources["currentUser"] as UserModel;
-        public SeriesCollection PieSeries { get; set; } = new SeriesCollection()
+        public SeriesCollection ProjectsPie { get; set; } = new SeriesCollection()
         {
             new PieSeries
             {
                 Title = "Not started",
-                Values = new ChartValues<ObservableValue> { new ObservableValue(20) },
+                Values = new ChartValues<ObservableValue> { new ObservableValue(0) },
                 DataLabels = true
             },
             new PieSeries
             {
                 Title = "In Progress",
-                Values = new ChartValues<ObservableValue> { new ObservableValue(20) },
+                Values = new ChartValues<ObservableValue> { new ObservableValue(0) },
                 DataLabels = true
             },
             new PieSeries
             {
                 Title = "Completed",
-                Values = new ChartValues<ObservableValue> { new ObservableValue(20) },
+                Values = new ChartValues<ObservableValue> { new ObservableValue(0) },
                 DataLabels = true
             }
         };
@@ -61,6 +60,28 @@ namespace teammy
         public ProgressReport()
         {
             InitializeComponent();
+            Parallel.Invoke(() =>
+            {
+                conn.Open();
+                try
+                {
+                    MySqlCommand getProjects = new MySqlCommand("SELECT Proj_Name FROM projects;", conn);
+
+
+                    MySqlDataReader reader = getProjects.ExecuteReader();
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            Dispatcher.Invoke(() => cmbProjects.Items.Add(reader[0]));
+                        }
+                    }
+                }
+                catch (MySqlException) { }
+                conn.Close();
+
+                cmbProjects.SelectedIndex = 0;
+            });
         }
         #endregion
 
@@ -134,6 +155,53 @@ namespace teammy
         {
             Hide();
             (Application.Current.Resources["mainInstance"] as Window).Show();
+        }
+
+        private void cmbTeams_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Parallel.Invoke(() => 
+            {
+                int nsCount = 0, ipCount = 0, coCount = 0;
+
+                conn.Open();
+                try
+                {
+                    MySqlCommand getTasks = new MySqlCommand("SELECT progress_code FROM tasks NATURAL JOIN projects WHERE Proj_Name = @projectName;", conn);
+                    getTasks.Parameters.AddWithValue("projectName", cmbProjects.SelectedItem.ToString());
+                    MySqlDataReader reader = getTasks.ExecuteReader();
+
+                    using (reader)
+                    {
+                        string progress;
+                        while (reader.Read())
+                        {
+                            progress = reader[0].ToString();
+                            if (progress.Equals("NS"))
+                            {
+                                nsCount++;
+                            }
+                            else if (progress.Equals("IP"))
+                            {
+                                ipCount++;
+                            }
+                            else
+                            {
+                                coCount++;
+                            }
+                        }
+                    }
+                }
+                catch (MySqlException m) 
+                {
+                    MessageBox.Show(m.Message, "MySqlException Thrown", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                ProjectsPie[0].Values = new ChartValues<ObservableValue> { new ObservableValue(nsCount) };
+                ProjectsPie[1].Values = new ChartValues<ObservableValue> { new ObservableValue(ipCount) };
+                ProjectsPie[2].Values = new ChartValues<ObservableValue> { new ObservableValue(coCount) };
+
+                conn.Close();
+            });
         }
     }
 }
