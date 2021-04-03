f11dee7238a5e80a68072cc203e41d0a203b070b
Anirudh Babu <anirudhbabu2001@gmail.com>
2021-04-03 11:50:43 -0400

    Empty Page Layout done
    Menu for navigation is available but would not work at this stage due to
    the absence of a logged in user.

---
+++ teammy/teammy/Schedule.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace teammy
+{
+    /// <summary>
+    /// Interaction logic for Schedule.xaml
+    /// </summary>
+    public partial class Schedule : Window
+    {
+        private static ResourceDictionary globalItems = Application.Current.Resources;
+
+        public Schedule()
+        {
+            InitializeComponent();
+        }
+        #region Title Bar Button Event Handlers
+        /// <summary>
+        ///     Shuts down the application
+        /// </summary>
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        ///     Minimizes the current window
+        /// </summary>
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        ///     Displays page menu when button is clicked
+        /// </summary>
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ContextMenu cm = FindResource("cmButton") as ContextMenu;
+            cm.PlacementTarget = sender as Button;
+            cm.IsOpen = true;
+        }
+        #endregion
+
+        #region Title Pane Event Handlers
+
+        /// <summary>
+        ///     Moves the window along with the title pane when it is dragged
+        /// </summary>
+        private void pnlTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            DragMove();
+        }
+
+        /// <summary>
+        ///     The method sets the background of the Button it contains to the same color as if it were hovered 
+        ///     upon.
+        /// </summary>
+        /// <param name="sender">The MenuItem triggering this event</param>
+        private void MenuItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
+
+            //The Button whose background is to be set
+            Button btnIcon = MenuItem.Children[1] as Button;
+            btnIcon.Background = new SolidColorBrush(Colors.LightBlue) { Opacity = 0.7 };
+        }
+
+        /// <summary>
+        ///     The method sets the background of the Button it contains to the same color as if it lost focus.
+        /// </summary>
+        /// <param name="sender">The MenuItem triggering this event</param>
+        private void MenuItem_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //The Grid encompassing all the icon elements for the menu item
+            Grid MenuItem = (sender as MenuItem).Icon as Grid;
+
+            //The Button whose background is to be set
+            Button btnIcon = MenuItem.Children[1] as Button;
+            btnIcon.Background = null;
+        }
+        #endregion
+
+        #region Page Navigation
+        /// <summary>
+        ///     Redirects to Progress Report Page
+        /// </summary>
+        private void progMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            (globalItems["progReportInstance"] as Window).Show();
+            Hide();
+        }
+
+        /// <summary>
+        ///     Redirects to Boards Page
+        /// </summary>
+        private void boardsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            (globalItems["createProjInstance"] as Window).Show();
+            Hide();
+        }
+
+        /// <summary>
+        ///     Redirects to Home page
+        /// </summary>
+        private void homeMenuItem_click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            (globalItems["mainInstance"] as Window).Show();
+        }
+
+        /// <summary>
+        ///     Redirects to Teams page
+        /// </summary>
+        private void teamsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            (globalItems["teamsListInstance"] as Window).Show();
+        }
+        #endregion
+
+
+    }
+}
