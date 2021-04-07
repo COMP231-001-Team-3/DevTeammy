bc0ef167ba0a3c6ad038d3dac593bdadd05453b5
chaeeun kim <afafpo@naver.com>
2021-04-08 03:05:12 +0900

    TeamContactListDone

--- teammy/teammy/Windows/TeamsContactlist.xaml.cs
+++ teammy/teammy/Windows/TeamsContactlist.xaml.cs
@@ -24,7 +24,7 @@ namespace teammy
         private static ResourceDictionary globalItems = Application.Current.Resources;
         public user currentUser { get; set; } = globalItems["currentUser"] as user;
         public team currentTeam { get; set; }
-        string proname;
+        
 
         List<user> contactinfo;
          teammyEntities dbContext = new teammyEntities();
@@ -35,13 +35,14 @@ namespace teammy
 
         private void contactWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            teamnamelabel.Content = currentTeam.Team_Name;
             
             contactinfo = (from user in dbContext.users
                            join mate in dbContext.team_mates
                            on user.user_id equals mate.user_id
                            join teams in dbContext.teams
                            on mate.Team_ID equals teams.Team_ID
-                           where teams.Team_Name == proname
+                           where teams.Team_Name == currentTeam.Team_Name
                            select user).ToList();
 
 
