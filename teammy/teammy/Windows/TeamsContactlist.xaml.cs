c5fc7d2e4b6cdff22096494889f0933fbec423a8
Anirudh Babu <anirudhbabu2001@gmail.com>
2021-04-21 19:03:06 -0400

    Shifted DB

--- teammy/teammy/Windows/TeamsContactlist.xaml.cs
+++ teammy/teammy/Windows/TeamsContactlist.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Data;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 
 namespace teammy
@@ -45,8 +37,6 @@ namespace teammy
                               on mate.Team_ID equals teams.Team_ID
                            where teams.Team_Name == currentTeam.Team_Name
                            select user).ToList();
-
-
 
             dtgTeamMates.ItemsSource = contactinfo;
         }
